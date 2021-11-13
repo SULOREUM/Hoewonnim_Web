91664f5aa0046bcd58527ee5d5fc99f4369dcbce
Seyiul <63714074+Seyiul@users.noreply.github.com>
2021-11-14 01:51:29 +0900

    DetailBoardPage.vue

--- frontend/src/components/views/boards/DetailBoardPage.vue
+++ frontend/src/components/views/boards/DetailBoardPage.vue
@@ -1,36 +1,27 @@
 <template>
 
   <div class="container">
-    <h3>Details</h3>
-    <div class="AddWrap">
-      <form>
-        <table class="tbAdd">
-          <colgroup>
-            <col style="width: 15%" />
-            <col style="width: 75%" />
-          </colgroup>
-          <tr>
-            <th>제목</th>
-            <td>{{list.title}}</td>
-          </tr>
-          <tr>
-            <th>id</th>
-            <td>{{list.createdUser}}</td>
-          </tr>
-          <tr>
-            <th>내용</th>
-            <td>{{list.content}}</td>
-          </tr>
-        </table>
-      </form>
+    <div class="ListWrap">
+      <div class="title">{{list.title}}</div>
+      <div class="info">
+        <div class="user">{{list.createdUser}}</div>
+        <div class="date">{{list.createdAt.substring(0,10)+' '+list.createdAt.substring(11,19)}}</div>
+      </div>
+      <div class="cont">
+        {{list.content}}
+      </div>
+      <div class="tagWrap">
+        <font-awesome-icon icon="fa-solid fa-tags" />
+        <div class="tag" v-if="list.tag !== ''&& list.tag !=='all'">#{{list.tag}}</div>
+      </div>
+
     </div>
 
     <div class="btnWrap">
       <input type="button" value="삭제" class="btn" @click =Del>
-      <router-link :to="{ name: 'List' }"><input type="button" value="돌아가기" class="btn"></router-link>
+      <input type="button" value="돌아가기" class="btn" @click =fnList>
     </div>
   </div>
-
 
 </template>
 
@@ -57,84 +48,71 @@ export default {
   }
 };
 </script>
-
 <style scoped>
-
 .container{
-  width: 100%;
-  height: 100%;
-  margin-top: 0px;
+  padding: 0px 50px;
 }
-.tbAdd {
-  width: 90%;
+.ListWrap{
+  width: 100%;
+  margin-bottom: 20px;
+}
+.title{
+  text-align: left;
+  padding: 10px 20px;
+  font-size: 1.2em;
 }
 
-.tbAdd td {
-  padding: 10px 10px;
-  box-sizing: border-box;
+.info{
+  padding: 10px 20px;
+  display: flex;
+  text-align: left;
+  font-size: 0.5em;
+}
+.user{
+  margin-right: 10px;
+
+}
+.date{
+
+}
+.cont{
+  min-height: 500px;
+  border: 1px solid  rgba(96, 117, 235, 0.28);
+  padding: 20px;
+  margin: 10px;
   text-align: left;
 }
-
-.tbAdd td input {
-  width: 100%;
-  min-height: 30px;
-  box-sizing: border-box;
-  padding: 0 10px;
-}
-
-.tbAdd td input[type=button]{
-  width: auto;
-  margin: auto 10px;
-  font-size: .6em;
-
-  border: 1px solid #7994db;
-  background-color: white;
-  color: #7994db;
-  border-radius: 40px;
-}
-
-.tbAdd td .button:hover{
-  background-color: #7994db;
-  color: white;
-}
-
-.tbAdd td .button.active{
-  background-color: #7994db;
-  color: white;
-}
-
-.btnWrap {
-  text-align: center;
-  margin: 20px auto;
+.tagWrap{
   display: flex;
-  justify-content: center;
-
+  align-items: center;
+  font-size: 0.8em;
+  padding: 0px 20px;
 }
-
-.btnWrap a {
-  margin: 0 10px;
+.tag{
+  width: 50px;
+  padding: 3px;
+  border-radius: .5em;
+  background-color: #7994DB;
+  color: white;
+  margin-left: 10px;
 }
-
+.btnWrap{
+}
+/*}*/
 .btn{
   width: 100px;
+  margin: 0px 10px;
   padding: 3px;
   border: 1px solid #aaa;
   background: white;
   border-radius: .5em;
   box-shadow: 0 1px 0 1px rgba(0, 0, 0, .04);
-}
-
-.btnAdd {
-  background:#7994db;
-  color: white;
+  cursor: pointer;
 }
 
 
 @media(max-width:767px) {
 
-  .tbAdd {
-    font-size: .8em;
-  }
 
   .btnWrap {
     text-align: center;
