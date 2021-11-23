bfeb457533f4c0890216fe01ca68e558aeee5073
chosiyeon <tldusdk5@naver.com>
2021-11-23 12:03:29 +0900

    update mypage.vue & infopage.vue

--- frontend/src/components/views/MyPageUserInfo.vue
+++ frontend/src/components/views/MyPageUserInfo.vue
@@ -1,22 +1,4 @@
 <template>
-  <!--  <div>-->
-  <!--    <div class="content">-->
-  <!--      <h3><strong>사용자 정보</strong></h3>-->
-  <!--      <h5>개인 정보를 수정할 수 있습니다.</h5>-->
-  <!--      <table class="userSettingTable">-->
-  <!--        <colgroup>-->
-  <!--          <col style="width:30%">-->
-  <!--          <col style="width:70%">-->
-  <!--        </colgroup>-->
-  <!--        <tr>-->
-  <!--          <td class="txt_left">프로필 사진</td>-->
-  <!--          <td>-->
-  <!--            <div class="userImage"></div>-->
-  <!--          </td>-->
-  <!--        </tr>-->
-  <!--      </table>-->
-  <!--    </div>-->
-  <!--  </div>-->
   <div class="container">
     <div class="content">
       <div class="container_inside">
@@ -26,32 +8,126 @@
         <div class="sub_title">
           프로필 및 정보를 수정하실 수 있습니다.
         </div>
+        <div class="user_info">
+          <div class="user_info_inside">
+            <table id="table" class="table">
+              <colgroup>
+                <col style="width:20%">
+                <col style="width:70%">
+                <col style="width:10%">
+              </colgroup>
+              <tr>
+                <th class="line"> 사진</th>
+                <td class="line">  </td>
+              </tr>
+              <tr>
+                <th class="line"> 닉네임</th>
+                <td class="line"> 술오름</td>
+              </tr>
+              <tr>
+                <th class="line"> 생년월일</th>
+                <td class="line"> 2018-01-01 </td>
+              </tr>
+              <tr>
+                <th class="line"> 성별</th>
+                <td class="line">  여</td>
+              </tr>
+              <tr>
+                <th class="line"> 챌린지</th>
+                <td class="line"> 다이어트</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+        <div class="contact_info">
+          <div class="container_inside">
+            <div class="user_info_inside">
+              <table id="contact_table" class="table">
+                <colgroup>
+                  <col style="width:20%">
+                  <col style="width:70%">
+                  <col style="width:10%">
+                </colgroup>
+                <tr>
+                  <th class="line"> 이메일</th>
+                  <td class="line">  suloreum@naver.com</td>
+                </tr>
+                <tr>
+                  <th class="line"> 전화번호</th>
+                  <td class="line"> 010-1234-5678 </td>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div class="etc_info">
+
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import $ from "jquery";
+
 export default {
-  name: "userInfo"
+  name: "userInfo",
+  mounted() {
+    $(document).ready(function(){
+      changeColor();
+      clickTd();
+      clickTr();
+    })
+    function changeColor(){
+      $('#table tr').mouseover(function(){
+        $(this).addClass('changeColor');
+      }).mouseout(function() {
+        $(this).removeClass('changeColor');
+      });
+      $('#contact_table tr').mouseover(function(){
+        $(this).addClass('changeColor');
+      }).mouseout(function() {
+        $(this).removeClass('changeColor');
+      });
+    }
+    function clickTd(){
+      $("#table tr td").click(function(){
+        // var text = $(this).text();
+        // alert(text);
+      });
+    }
+    function clickTr(){
+      $("#table tr").click(function(){
+        // var text = $(this).text();
+        // alert(text);
+      });
+    }
+  }
 }
+
+
 </script>
 
 <style scoped>
 
 @import url('https://fonts.googleapis.com/css2?family=Gothic+A1:wght@700&display=swap');
 
+.changeColor {
+  background-color: #eeeeee;
+}
+
 .container {
-  background: rgb(230, 232, 233);
+  /*background: rgb(230, 232, 233);*/
+  font-size: 1em;
 }
 
 .content {
   position: relative;
   background-color: white;
-  width: 60%;
+  width: 50%;
   height: 1200px;
-  margin-left: 20%;
-
+  margin-left: 25%;
 }
 
 .container_inside {
@@ -76,39 +152,80 @@ export default {
   font-family: 'Gothic A1', sans-serif;
 }
 
+.user_info {
+  position: relative;
+  margin-top: 20px;
+  width: 100%;
+  /*height: 35%;*/
+  border-radius: 16px;
+  border: 1px solid lightgray;
+  padding-top: 40%;
+}
+
+.user_info_inside {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.table {
+  width: 100%;
+  height: 100%;
+}
+
+.table th {
+  color: black;
+  font-size: 0.7em;
+}
+
+.table tr {
+
+}
+
+.line {
+  border-bottom: 1px solid lightgray;
+}
+
+.contact_info {
+  position: relative;
+  margin-top: 10px;
+  width: 100%;
+  padding-top: 25%;
+  border-radius: 16px;
+  border: 1px solid lightgray;
+}
+
+.etc_info {
+  position: relative;
+  margin-top: 10px;
+  width: 100%;
+  padding-top: 30%;
+  border-radius: 16px;
+  border: 1px solid lightgray;
+}
+
+@media all and (min-width: 768px) and (max-width: 1023px) {
+  .container {
+    font-size: 0.8em;
+  }
+
+  .content {
+    width: 80%;
+    margin-left: 10%;
+  }
+}
+
 @media (max-width: 767px) {
   .container {
     font-size: 0.7em;
   }
+
+  .content {
+    width: 80%;
+    margin-left: 10%;
+  }
 }
 
-/*.content {*/
-/*  width: 60%;*/
-/*  height: 1200px;*/
-/*  margin-left: 20%;*/
-/*  background-color: silver;*/
-/*  text-align: left;*/
-/*}*/
-/*.userSettingTable {*/
-/*  width: 100%;*/
-/*}*/
-/*.userSettingTable tr {background-color: #22a8d8;height: 240px}*/
-/*.userSettingTable th, .userSettingTable td{*/
-/*  border-bottom:1px solid #eee;*/
-/*  padding:10px;*/
-/*  font-size: 18px;*/
-/*}*/
-/*.userSettingTable td.txt_left{*/
-/*  margin-left: 10px;*/
-/*  text-align: left;*/
-/*  font-size: 15px;*/
-/*  font-weight: bold;*/
-/*}*/
-/*.userImage {*/
-/*  background-color: red;*/
-/*  height: 120px;*/
-/*  width: 120px;*/
-/*  margin-left: 3%;*/
-/*  margin-top: 1%;*/
-/*}*/
 </style>
