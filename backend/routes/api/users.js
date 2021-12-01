0d02f683dfe332ded766fefe4f23d6382b4b9ca8
chosiyeon <tldusdk5@naver.com>
2021-12-01 23:30:40 +0900

    change user function

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,70 +1,82 @@
 const express = require('express');
 const mongodb = require('mongodb');
+
 const router = express.Router();
 
 // Get Users
 router.get('/', async (req, res) => {
-    const users = await loadUsersCollections();
-    res.send(await users.find({}).sort({createdAt:-1}).toArray());
+    const posts = await loadPostsCollections();
+    res.send(await posts.find({}).sort({createdAt:-1}).toArray());
 });
 
 router.get('/:id', async function(req, res){
     // const find_id = parseInt(req.params.id, 10);
     const find_id = mongodb.ObjectID(req.params.id);
-    const users = await loadUsersCollections();
-    res.send(await users.find({_id:find_id}).toArray());
+    const posts = await loadPostsCollections();
+    res.send(await posts.find({_id:find_id}).toArray());
 });
 
-// Add Users
-router.post('/', async (req, res)=> {
-    const users = await loadUsersCollections();
-    // 이름, 성별, 아이디, 비밀번호, 이메일, 로그인여부, 챌린지, 체중, 선호종목
-
-    await users.insertOne({
-
-        id: req.body.data.id,
+// Add User
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollections();
+    await posts.insertOne({
+        id: req.body.id,
         password: req.body.data.password,
         name: req.body.data.name,
-        gender: req.body.data.gender,
+        age: req.body.data.age,
+        state: req.body.data.data.state,
+        sex: req.body.data.sex,
+        profile_image: req.body.data.profile_image,
+        birth: req.body.data.birth,
         phone: req.body.data.phone,
-        // mail: req.body.data.mail,
-        // login: req.body.data.login,
-        // challenge: req.body.data.challenge,
-        // weight: req.body.data.weight,
-        // preference: req.body.data.preference
+        mail: req.body.data.mail,
+        interest: req.body.data.interest,
+        challenge: req.body.data.challenge,
+        weight: req.body.data.weight,
+        liked_post: req.body.data.liked_post
     });
-    res.status(202).send();
+    res.status(201).send();
 });
 
 router.post('/:id', async (req, res) => {
-    const users = await loadUsersCollections();
-    users.update(
+    const post = await loadPostsCollections();
+    post.update(
         {_id: mongodb.ObjectID(req.params.id)},
         {
-            $push: {
-                id: req.body.data.id,
+            $set: {
+                id: req.body.id,
                 password: req.body.data.password,
                 name: req.body.data.name,
-                gender: req.body.data.gender,
+                age: req.body.data.age,
+                state: req.body.data.data.state,
+                sex: req.body.data.sex,
+                profile_image: req.body.data.profile_image,
+                birth: req.body.data.birth,
                 phone: req.body.data.phone,
-                // mail: req.body.data.mail,
-                // login: req.body.data.login,
-                // challenge: req.body.data.challenge,
-                // weight: req.body.data.weight,
-                // preference: req.body.data.preference
+                mail: req.body.data.mail,
+                interest: req.body.data.interest,
+                challenge: req.body.data.challenge,
+                weight: req.body.data.weight,
+                liked_post: req.body.data.liked_post
             }
         }
     );
-    res.status(202).send();
+    res.status(201).send();
 });
 
-async function loadUsersCollections() {
-    const client = await mongodb.MongoClient.connect('mongodb+srv://lx97:79xl@hoewonnimcluster.6jzln.mongodb.net/Hoewonnim', {
+// Delete User
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollections();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+    res.status(200).send();
+});
+
+async function loadPostsCollections() {
+    const client = await mongodb.MongoClient.connect('***REMOVED***', {
         useNewUrlParser: true
     })
+
     return client.db('Hoewonnim').collection('users');
 }
 
-
-//
 module.exports = router;
--- frontend/src/services/users/getUserInfo.js
+++ frontend/src/services/users/getUserInfo.js
@@ -4,9 +4,8 @@ const url = 'http://localhost:3000/api/users/';
 
 class getUserInfo {
     // Get User Information
-    static getUser(id) {
+    static getUsers() {
         const promise = new Promise(resolve => {
-            console.log(id)
             const res = axios.get(url)
             resolve(res)
         })
