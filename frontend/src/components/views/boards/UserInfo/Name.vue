876c784895843a46acbe38694ceaf9c3b1a13df3
chosiyeon <tldusdk5@naver.com>
2021-12-02 22:18:01 +0900

    modify user name UI

--- frontend/src/components/views/boards/UserInfo/Name.vue
+++ frontend/src/components/views/boards/UserInfo/Name.vue
@@ -8,6 +8,18 @@
         <div class="sub_title">
           닉네임을 수정하실 수 있습니다.
         </div>
+        <div class="name_revise">
+          <div class="name_revise_content">
+            <div class="name_area">
+              <input v-model="username" id="username" type="text" placeholder="성"/>
+              <input v-model="username2" id="username2" type="text" placeholder="이름"/>
+            </div>
+            <div class="btn_area">
+              <input id="ok_btn" type="submit" value="확인"/>
+              <input id="cancle_btn" type="submit" value="취소"/>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -38,8 +50,7 @@ name: "Name",
 .content {
   position: relative;
   background-color: white;
-  width: 50%;
-  height: 1200px;
+  width: 40%;
   margin-left: 25%;
 }
 
@@ -66,6 +77,105 @@ name: "Name",
 }
 
 .name_revise {
+  position: relative;
+  width:500px;
+  margin-top: 20px;
+  border-radius: 16px;
+  border: 1px solid lightgray;
+}
 
+.name_revise:before {
+  content: "";
+  display: block;
+  padding-top: 60%;
+}
+
+.name_revise_content {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
+
+.name_area {
+  width: 100%;
+  height: 65%;
+}
+
+.btn_area {
+  width: 75%;
+  height: 30%;
+  margin-top: 5%;
+}
+
+/* input */
+#username {
+  width: 70%;
+  height: 50px;
+  margin-top: 20px;
+  margin-right: 25%;
+  float: right;
+  border-radius: 2px;
+  border: 1px solid lightgray;
+}
+
+#username2 {
+  width: 70%;
+  height: 50px;
+  margin-top: 20px;
+  margin-right: 25%;
+  float: right;
+  border-radius: 2px;
+  border: 1px solid lightgray;
+}
+
+#cancle_btn {
+  width: 25%;
+  height: 40px;
+  float: right;
+  margin-right: 20px;
+  color: #fff;
+  background-color: #7994DB;
+  border: 0;
+  cursor: pointer;
+  border-radius: 2px;
+}
+
+#ok_btn {
+  width: 25%;
+  height: 40px;
+  float: right;
+  color: #fff;
+  background-color: #7994DB;
+  border: 0;
+  cursor: pointer;
+  border-radius: 2px;
+}
+
+@media all and (min-width: 768px) and (max-width: 1023px) {
+  .container {
+    font-size: 0.8em;
+  }
+
+  .content {
+    width: 80%;
+    margin-left: 10%;
+  }
+}
+
+@media (max-width: 767px) {
+  .container {
+    font-size: 0.7em;
+  }
+
+  .content {
+    width: 80%;
+    margin-left: 10%;
+  }
+
+  .name_revise {
+    width: 400px
+  }
 }
 </style>
